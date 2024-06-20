4e50bb6eb7af848743ed7b0ffc1eb04f1b511b43
Dekarter <dekarter@protonmail.com>
2024-06-20 21:24:10 +0300

    bot switched from client to bot model

--- bot.py
+++ bot.py
@@ -3,36 +3,34 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
+from music_cog import music_cog
 
 
 def run_bot():
     intents = discord.Intents.default()
     intents.message_content = True
-    client = discord.Client(intents=intents)
-    tree = app_commands.CommandTree(client)
-    client.tree = tree
+    bot = commands.Bot(command_prefix='!', intents=intents)
 
-    @client.event
+    @bot.event
     async def on_ready():
         print('Logged on')
-        await client.tree.sync()
 
-    @client.tree.command(name="hello", description="Says hello to you ^_^")
+    @bot.tree.command(name="hello", description="Says hello to you ^_^")
     async def hello(interaction: discord.Interaction):
         await interaction.response.send_message(f"Hello {interaction.user.mention}")
 
-    @client.tree.command(name="sync")
-    async def sync(interaction: discord.Interaction):
-        if interaction.user.id == os.getenv("ADMIN_ID"):
-            await client.tree.sync()
+    @bot.command(name="sync")
+    async def sync(ctx):
+        if ctx.author.id == int(os.getenv("ADMIN_ID")):
+            await bot.tree.sync()
             print("Synced!")
         else:
-            await interaction.response.send_message(f"ЖОПА")
+            await ctx.send("ZHOPA")
 
-    @tree.command(name="ping", description="Shows bot's latency in ms.")
+    @bot.tree.command(name="ping", description="Shows bot's latency in ms.")
     async def ping(interaction: discord.Interaction):
-        bot_latency = round(client.latency * 1000)
+        bot_latency = round(bot.latency * 1000)
         await interaction.response.send_message(f"Pong! {bot_latency} ms.")
 
     load_dotenv()
-    client.run(os.getenv("DISCORD_TOKEN"))
+    bot.run(os.getenv("DISCORD_TOKEN"))
